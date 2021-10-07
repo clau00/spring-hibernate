6d05062eb5ffa4f1e5e2463ed627fece3d29cddd
claudiu <danbalint_88@yahoo.com>
2021-10-07 22:03:05 +0300

    CB: Hibernate Advanced Mappings: @ManyToMany: added course student relationship

--- hb-05-many-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -25,6 +25,15 @@ public class Course {
 	@JoinColumn(name = "course_id")
 	private List<Review> reviews;
 
+	@ManyToMany(fetch = FetchType.LAZY,
+			cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinTable(
+			name = "course_student",
+			joinColumns = @JoinColumn(name = "course_id"),
+			inverseJoinColumns = @JoinColumn(name = "student_id")
+	)
+	private List<Student> students;
+
 	public Course() {}
 
 	public Course(String title) {
@@ -63,12 +72,28 @@ public class Course {
 		this.reviews = reviews;
 	}
 
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
 	public void addReview(Review review) {
 		if(reviews == null) {
 			reviews = new ArrayList<>();
 		}
 
 		reviews.add(review);
+	}
+
+	public void addStudent(Student student) {
+		if(students == null) {
+			students = new ArrayList<>();
+		}
+
+		students.add(student);
 	}
 
 	@Override
