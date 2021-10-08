62682febd1d8aa92f1b99516edac38166c0b0e05
claudiu <danbalint_88@yahoo.com>
2021-10-08 10:12:53 +0300

    CB: Hibernate Advanced Mappings: @ManyToMany: deleted a student, this also deleted the relationship from join table

---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/DeleteStudentDemoApp.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.*;
+
+public class DeleteStudentDemoApp {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get the student from db
+			int studentId = 2;
+			Student student = session.get(Student.class, studentId);
+
+			System.out.println("\nLoaded student: " + student);
+			System.out.println("Courses: " + student.getCourses());
+
+			// delete student
+			System.out.println("\nDeleting student...");
+			session.delete(student);
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("\nDone!");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
